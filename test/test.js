40ef26c0f7ffd23a2d149a4ee6f8256fc6a5a87b
Eeo Jun <packwolf58@gmail.com>
2015-12-19 00:31:04 +0800

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-suite('evee', function() {
+describe('evee', function() {
   var $ = document.getElementById.bind(document);
   var data = {r: 0};
   var elem = $('test');
@@ -10,30 +10,44 @@ suite('evee', function() {
     assert(ev.target === elem);
   };
 
-  test('evee#trigger works for normal events', function() {
-    evee.bind(elem, 'focus', handler, data);
-    evee.trigger(elem, 'focus');
-    assert(data.r === 1);
-  });
-
-  test('evee#trigger works properly for mouse events', function() {
-    evee.bind(elem, 'click', handler, data);
-
-    evee.trigger(elem, 'click');
-    assert(data.r === 2);
-  });
-
-  test('evee#unbind can unbind handlers', function() {
-    evee.unbind(elem, 'click', handler);
-    evee.trigger(elem, 'click');
-    assert(data.r === 2);
-  });
-
-  test('evee#trigger triggers external handlers as well', function() {
-    elem.addEventListener('click', function() {
-      data.e = 1;
+  describe('evee#trigger', function() {
+    it('works for normal events', function() {
+      evee.bind(elem, 'focus', handler, data);
+      evee.trigger(elem, 'focus');
+      assert(data.r === 1);
     });
-    evee.trigger(elem, 'click');
-    assert(data.e === 1);
+
+    it('works for mouse events', function() {
+      evee.bind(elem, 'click', handler, data);
+
+      evee.trigger(elem, 'click');
+      assert(data.r === 2);
+    });
+
+    it('triggers external handlers', function() {
+      elem.addEventListener('click', function() {
+        data.e = 1;
+      });
+      evee.trigger(elem, 'click');
+      assert(data.e === 1);
+    });
+  });
+
+  describe('evee#unbind', function() {
+    it('unbinds handlers', function() {
+      evee.unbind(elem, 'click', handler);
+      evee.trigger(elem, 'click');
+      assert(data.r === 3);
+    });
+
+    it('unbinds external handlers', function() {
+      var f = function(ev) {
+        ev.data.r += 1;
+      };
+      evee.bind(elem, 'click', f);
+      evee.unbind(elem, 'click', f);
+      evee.trigger(elem, 'click');
+      assert(data.r === 3);
+    });
   });
 });
