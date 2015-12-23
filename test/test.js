3e737d861273afb5730407a600ba0e2bad95817d
Eeo Jun <packwolf58@gmail.com>
2015-12-23 20:58:27 +0800

    Rewrite core tests

--- test/test.js
+++ test/test.js
@@ -1,42 +1,47 @@
-describe('evee', function() {
-  var data = {};
-  var elem = $('test');
+describe('evee.on', function() {
+  var el = $('p');
 
-  var handler = function(ev) {
-    data.r = 1;
-    assert(ev.target === elem);
+  it('returns the handler', function() {
+    var fn = function() {};
+    assert(evee.on(el, 'click', fn) === fn);
+  });
+
+  it('binds the handler to the element', function(done) {
+    evee.on(el, 'click', function() {
+      done();
+    });
+    evee.fire(el, 'click');
+  });
+});
+
+describe('evee.off', function() {
+  var el = $('p');
+
+  it('removes the event handler', function() {
+    var fn = evee.on(el, 'click', function() {
+      assert(false);
+    });
+    evee.off(el, 'click', fn);
+    evee.fire(el, 'click');
+  });
+});
+
+describe('evee.fire', function() {
+  var el = $('p');
+  var ev = {
+    mouse: 'click',
+    keyboard: 'keyup',
+    html: 'focus',
+    custom: 'custom',
   };
 
-  afterEach(function() {
-    data = {};
-  });
-
-  describe('#fire', function() {
-    it('works for normal events', function() {
-      evee.on(elem, 'focus', handler);
-      evee.fire(elem, 'focus');
-      assert(data.r === 1);
+  for (var i in ev) {
+    var type = ev[i];
+    it('works for ' + i + ' events', function(done) {
+      evee.on(el, type, function() {
+        done();
+      }); 
+      evee.fire(el, type);
     });
-
-    it('works for mouse events', function() {
-      evee.on(elem, 'click', handler);
-      evee.fire(elem, 'click');
-      assert(data.r === 1);
-    });
-
-    it('works for keyboard events', function() {
-      evee.on(elem, 'keydown', handler);
-      evee.fire(elem, 'keydown');
-      assert(data.r === 1);
-    });
-  });
-
-  describe('#off', function() {
-    it('unbinds handlers', function() {
-      evee.on(elem, 'click', handler);
-      evee.off(elem, 'click', handler);
-      evee.fire(elem, 'click');
-      assert(!data.r);
-    });
-  });
+  }
 });
