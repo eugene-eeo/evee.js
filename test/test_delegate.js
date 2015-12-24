ade61cca6a6cd5bc9dd023512fbee90a1fe43f4c
Eeo Jun <packwolf58@gmail.com>
2015-12-24 16:35:33 +0800

    Rewrite tests for evee.delegate

--- test/test_delegate.js
+++ test/test_delegate.js
@@ -1,69 +1,52 @@
 describe('evee.delegate', function() {
   var el = $('div');
-  var children = [
-    $('a'),
-    $('a'),
-    $('a'),
-  ];
+  el.innerHTML = (
+    "<a></a>"
+     + "<a><b id='id'></b></a>"
+     + "<a><b class='klass'></b></a>"
+  );
+  var children = [];
+  for (var i=3; i--;)
+    children.unshift(el.children[i]);
+
+  var b_id = children[1].firstChild;
+  var b_kl = children[2].firstChild;
 
   // Workaround for safari - event handlers are not fired if
   // they are not bound to a "real" node.
   document.body.appendChild(el);
 
-  for (var i=children.length; i--;)
-    el.appendChild(children[i]);
-
   it('binds the event handler to the parent element', function(done) {
-    evee.delegate(el, 'click', 'a', function() {
+    evee.delegate(el, 'click', '#id', function() {
       done();
     });
-    evee.fire(children[0], 'click');
+    evee.fire(b_id, 'click');
   });
 
   it('returns the event handler', function() {
-    var h = evee.delegate(el, 'focus', 'a', function() {
+    var h = evee.delegate(el, 'click', 'a', function() {
       assert(false);
     });
-    evee.fire(el, 'focus');
-    evee.off(el, 'focus', h);
+    evee.off(el, 'click', h);
+    evee.fire(children[0], 'click');
   });
 
   it('calls the handler with the event and target', function() {
-    var data = {};
-    var h = evee.delegate(el, 'focus', 'a', function(ev, t) {
-      assert(t === ev.target);
-      assert(t === data.el);
+    evee.delegate(el, 'focus', 'a', function(ev, t) {
+      assert(t === children[2]);
     });
-    for (var i=children.length; i--;) {
-      var e = children[i];
-      data.el = e;
-      evee.fire(e, 'focus');
-    }
-    evee.off(el, 'focus', h);
+    evee.fire(b_kl, 'focus');
   });
 
-  it('will not fire if the selector does not match', function() {
-    evee.delegate(el, 'keyup', '#id', function() {
-      assert(false);
-    });
-    evee.fire(children[0], 'keyup');
-  });
-
-  it('will only call the handler once', function() {
-    var c1 = children[0];
-    var c2 = $('a');
+  it('only calls the handler once', function() {
     var counter = 0;
-
-    c1.appendChild(c2);
-
-    evee.delegate(el, 'keydown', '*', function() {
+    evee.delegate(el, 'keydown', '*', function(ev, t) {
       counter++;
     });
-
-    evee.fire(c2, 'keydown');
+    evee.fire(b_id, 'keydown');
     assert(counter === 1);
   });
-
+ 
   after(function() {
     document.body.removeChild(el);
   });
