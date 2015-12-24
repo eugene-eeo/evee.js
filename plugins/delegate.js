8135a2cb4b960d01625d6bcb3669d5e49a381148
Eeo Jun <packwolf58@gmail.com>
2015-12-24 13:32:56 +0800

    Replace while loop with for loop

--- plugins/delegate.js
+++ plugins/delegate.js
@@ -10,12 +10,9 @@ evee.delegate = (function() {
 
   return function(el, type, selector, fn) {
     return evee.on(el, type, function(ev) {
-      var node = ev.target;
-      while (node !== el) {
+      for (var node = ev.target; node !== el; node = node.parentNode)
         if (matchesSelector.call(node, selector))
           return fn(ev, node);
-        node = node.parentNode;
-      }
     });
   };
 })();
