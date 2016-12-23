e277ce9c2109bf93cd839d463153dd1c2d745b27
Eeo Jun <141bytes@gmail.com>
2016-12-23 10:42:40 +0800

    fix node is null error

--- plugins/delegate.js
+++ plugins/delegate.js
@@ -10,7 +10,7 @@
 
   evee.delegate = function(el, type, selector, fn) {
     return evee.on(el, type, function(ev) {
-      for (var node = ev.target; node !== el; node = node.parentNode)
+      for (var node = ev.target; node && node !== el; node = node.parentNode)
         if (node.nodeType === 1 && matchesSelector.call(node, selector))
           return fn(ev, node);
     });
